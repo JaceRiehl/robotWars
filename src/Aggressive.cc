d98e0ed1d8e8f8ba81e8643be69035429db424d6
JaceRiehl <jace.riehl@gmail.com>
2017-03-24 21:57:00 -0600

    Aggressive and RoundRobin Tests working

--- src/Aggressive.cc
+++ src/Aggressive.cc
@@ -1,5 +1,5 @@
 #include "Aggressive.h"
-
+#include <iostream>
 int Aggressive::attack(vector<Parts> p)
 {
  int damage = 0;
@@ -19,15 +19,22 @@ int Aggressive::attack(vector<Parts> p)
         rechargeTimes[i] = rechargeTimes[i] - 1;
     }
 
-
+    int index = -1;
 
     for(int i = 0; i<parts.size(); i++)
     {
-        if(parts[i].getDamage() > damage && rechargeTimes[i] == 0)
+        if(rechargeTimes[i] == 0)
         {
-            damage = parts[i].getDamage();
-            rechargeTimes[i] = parts[i].getRechargeTime();
+            if(damage < parts[i].getDamage())
+            {
+            index = i;
+            }
         }
+    }
+    if(index != -1)
+    {
+    damage = parts[index].getDamage();
+    rechargeTimes[index] = parts[index].getRechargeTime();
     }
 
    return damage;
